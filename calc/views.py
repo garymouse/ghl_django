f3767000aedc46eda4bd58a0cb0d62a6d9532546
Yilong Ren <yilongx.ren@intel.com>
2016-11-28 10:59:18 +0800

    url test

--- calc/views.py
+++ calc/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.shortcuts import render 
+from django.core.urlresolvers import reverse
+ 
+def index(request):
+    return render(request, 'home.html')
 
 def add(request):
-    a = request.GET("a")
-    b = request.GET("b")
+    a = request.GET.get('a',0)
+    b = request.GET.get('b',0)
     c = int(a) + int(b)
     return HttpResponse(str(c))
 
+def add2(request, a, b):
+    c = int(a) + int(b)
+    return HttpResponse(str(c))
+    
+
+def old_add2(request, a, b):
+    return HttpResponseRedirect(
+        reverse('add2', args=(a, b))
+    )
--- ghl_django/urls.py
+++ ghl_django/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from calc import views as calc_views
 
 urlpatterns = [
+    url(r'^$', calc_views.index, name="home"),
     url(r'^add/$', calc_views.add, name="add"),
+    url(r'^add/(\d+)/(\d+)/$', calc_views.old_add2),
+    url(r'^add_new/(\d+)/(\d+)/$', calc_views.add2, name="add2"),
     url(r'^admin/', admin.site.urls),
 ]
